00514724b15c1f6b5e09589b75ead182f8202bc6
NamesMark <markfirn@gmail.com>
2023-05-08 18:19:49 +0300

    initial (everything working)

---
+++ src/app.js
@@ -0,0 +1,46 @@
+import init, { calculate_vicz_expression } from "./rust_viczesimal/pkg/rust_viczesimal.js";
+
+
+export async function calculate_expression(expression) {
+    await init();
+    return calculate_vicz_expression(expression);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('input[type="button"]');
+  
+    buttons.forEach(button => {
+      button.addEventListener('click', async (e) => {
+        const action = e.target.dataset.action;
+  
+        switch (action) {
+            case 'calculate':
+                const result = await calculate_expression(calculator.display.value);
+                calculator.resultDisplay.value = result;
+                break;
+            case 'calculate+20':
+                calculator.display.value += ' + 10'
+                const result2 = await calculate_expression(calculator.display.value);
+                calculator.resultDisplay.value = result2;
+                break;
+            case 'calculate-20':
+                calculator.display.value += ' - 10'
+                const result3 = await calculate_expression(calculator.display.value);
+                calculator.resultDisplay.value = result3;
+                break;
+            case 'calculate*20':
+                calculator.display.value += ' * 10'
+                const result4 = await calculate_expression(calculator.display.value);
+                calculator.resultDisplay.value = result4;
+                break;
+            case 'calculate/20':
+                calculator.display.value += ' / 10'
+                const result5 = await calculate_expression(calculator.display.value);
+                calculator.resultDisplay.value = result5;
+                break;
+            default:
+                break;
+        }
+      });
+    });
+  });
---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { copyFileSync } from 'fs';
+import { resolve } from 'path';
+import fs from 'fs/promises';
+import path from 'path';
+
+const wasmPath = resolve(__dirname, 'src/rust_viczesimal/pkg');
+const buildWasmPath = resolve(__dirname, 'dist/rust_viczesimal/pkg');
+
+async function copyWasmFiles() {
+  const srcDir = 'src/rust_viczesimal/pkg';
+  const destDir = 'dist/rust_viczesimal/pkg';
+  const wasmFiles = ['rust_viczesimal_bg.wasm', 'rust_viczesimal.js'];
+
+  await fs.mkdir(destDir, { recursive: true });
+
+  for (const file of wasmFiles) {
+    const srcPath = path.join(srcDir, file);
+    const destPath = path.join(destDir, file);
+    await fs.copyFile(srcPath, destPath);
+  }
+}
+
+export default {
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: (id) => id.includes("rust_viczesimal/pkg"),
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  },
+  plugins: [
+    {
+      name: 'copy-wasm-files',
+      writeBundle() {
+        copyWasmFiles();
+      },
+    },
+  ],
+};
