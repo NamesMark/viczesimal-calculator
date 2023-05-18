4a741eefffb74e24613e3e74386a42315378209c
NamesMark <markfirn@gmail.com>
2023-05-18 12:52:27 +0300

    repeat of action implemented

--- src/app.js
+++ src/app.js
@@ -8,35 +8,53 @@ export async function calculate_expression(expression) {
 
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('input[type="button"]');
+    let previousExpression = '';
+    let previousResult = '';
   
     buttons.forEach(button => {
       button.addEventListener('click', async (e) => {
         const action = e.target.dataset.action;
-  
+ 
         switch (action) {
             case 'calculate':
+                let currentExpression = calculator.display.value;
+                let lastOperator = currentExpression.match(/[-+*/]/g).pop();
+                let lastNumber = currentExpression.split(lastOperator).pop();
+                if (currentExpression == previousExpression) {
+                    calculator.display.value = previousResult + lastOperator + lastNumber;
+                }
                 const result = await calculate_expression(calculator.display.value);
                 calculator.resultDisplay.value = result;
+                previousExpression = calculator.display.value;
+                previousResult = result;
                 break;
             case 'calculate+20':
                 calculator.display.value += ' + 10'
                 const result2 = await calculate_expression(calculator.display.value);
                 calculator.resultDisplay.value = result2;
+                previousExpression = calculator.display.value;
+                previousResult = result2;
                 break;
             case 'calculate-20':
                 calculator.display.value += ' - 10'
                 const result3 = await calculate_expression(calculator.display.value);
                 calculator.resultDisplay.value = result3;
+                previousExpression = calculator.display.value;
+                previousResult = result3;
                 break;
             case 'calculate*20':
                 calculator.display.value += ' * 10'
                 const result4 = await calculate_expression(calculator.display.value);
                 calculator.resultDisplay.value = result4;
+                previousExpression = calculator.display.value;
+                previousResult = result4;
                 break;
             case 'calculate/20':
                 calculator.display.value += ' / 10'
                 const result5 = await calculate_expression(calculator.display.value);
                 calculator.resultDisplay.value = result5;
+                previousExpression = calculator.display.value;
+                previousResult = result5;
                 break;
             default:
                 break;
